5f51912e819c4e5fca5f2eade747a143a3e1a3af
Stephen S. Mitchell <stephen@stephensmitchell.com>
2023-09-07 02:56:42 -0500

    09-07-2023 commit

---
+++ AlibreDesignInsight/SketchInsight/elliptical_shapes_001/5_30_06_PM_9_6_2023/CreateFolders.ps1
@@ -0,0 +1,24 @@
+New-Item -ItemType Directory -Name "ARCS" 
+New-Item -ItemType Directory -Name "ARC-UPDATES" 
+
+New-Item -ItemType Directory -Name "LINES" 
+New-Item -ItemType Directory -Name "LINE-UPDATES" 
+
+New-Item -ItemType Directory -Name "BSPLINES" 
+New-Item -ItemType Directory -Name "BSPLINE-UPDATES" 
+
+New-Item -ItemType Directory -Name "CIRCLE_ARCS" 
+New-Item -ItemType Directory -Name "CIRCLE_ARC-UPDATES" 
+
+New-Item -ItemType Directory -Name "CIRCLES" 
+New-Item -ItemType Directory -Name "CIRCLE-UPDATES" 
+
+New-Item -ItemType Directory -Name "ELLIPTICAL_ARCS" 
+New-Item -ItemType Directory -Name "ELLIPTICAL_ARC-UPDATES" 
+
+New-Item -ItemType Directory -Name "ELLIPSES" 
+New-Item -ItemType Directory -Name "ELLIPSE-UPDATES" 
+
+New-Item -ItemType Directory -Name "POINTS" 
+New-Item -ItemType Directory -Name "POINT-UPDATES" 
+
